ca6be9627279bf7996dafebf8ec1c39b85fc786d
th3chrisman <100044537+th3chrisman@users.noreply.github.com>
2022-02-19 16:08:49 -0600

    Create test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+
+
+import mysql.connector
+from mysql.connector.constants import ClientFlag
+
+config = {
+    'user': 'root',
+    'password': '',
+    'host': '35.193.3.52',
+    'client_flags': [ClientFlag.SSL],
+    'ssl_ca': 'ssl/server-ca.pem',
+    'ssl_cert': 'ssl/client-cert.pem',
+    'ssl_key': 'ssl/client-key.pem'
+}
+
+
+cnxn = mysql.connector.connect(**config)
